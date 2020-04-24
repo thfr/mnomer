5d45e480912726408bd71061c3345f7d75a16c0c
thfr <thfr.git@e.mail.de>
2020-04-25 00:36:21 +0200

    Make debug logging stand out

--- src/main.rs
+++ src/main.rs
@@ -14,19 +14,23 @@ fn main() {
     let freq = 500.0;
     let length = 0.002;
     let mut sine = AudioSignal::generate_sine(freq, length, 3);
-    println!("Sine {}Hz, {}s:", freq, length);
-    for sample in &sine.signal {
-        print!("{} ", sample);
+    {
+        println!("Sine {}Hz, {}s:", freq, length);
+        for sample in &sine.signal {
+            print!("{} ", sample);
+        }
+        println!();
     }
-    println!();
 
-    println!("Highpass filter with 20Hz and lowpass filter with 20kHz");
     sine.highpass_20hz();
     sine.lowpass_20khz();
-    for sample in &sine.signal {
-        print!("{} ", sample);
+    {
+        println!("Highpass filter with 20Hz and lowpass filter with 20kHz");
+        for sample in &sine.signal {
+            print!("{} ", sample);
+        }
+        println!();
     }
-    println!();
 
     let fade_time = 0.00025;
     match sine.fade_in_out(fade_time, fade_time) {
